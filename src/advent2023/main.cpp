81fd307c1ab9f86bd9c601e37c6ca305820457de
vlad <vladigr1@gmail.com>
2023-12-08 18:04:17 +0200

    sync: working example

---
+++ src/advent2023/main.cpp
@@ -0,0 +1,23 @@
+#include <fmt/printf.h>
+#include <spdlog/spdlog.h>
+
+#include <fmt/color.h>
+#include <iostream>
+
+int main()
+{
+  try {
+    std::cout << fmt::format(fg(fmt::color::crimson) | fmt::emphasis::bold, "Hello, {}!\n", "world") << std::endl;
+
+    std::cout << fmt::format(
+      fg(fmt::color::floral_white) | bg(fmt::color::slate_gray) | fmt::emphasis::underline, "Hello, {}!\n", "???")
+              << std::flush;
+
+    std::cout << fmt::format(fg(fmt::color::steel_blue) | fmt::emphasis::italic, "Hello, {}!\n", "??") << std::endl;
+    fmt::printf("hello world");
+    std::string a;
+    std::cin >> a;
+  } catch (const std::exception &e) {
+    spdlog::error("Unhandled exception in main: {}", e.what());
+  }
+}
