a49e96fe5d34552e5d11275f5a9a70cefe250109
vlad <vladigr1@gmail.com>
2023-12-08 19:44:31 +0200

    feature: day1 solution

---
+++ src/day1/main.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <algorithm>
+#include <fmt/format.h>
+#include <fmt/printf.h>
+#include <functional>
+#include <spdlog/spdlog.h>
+
+int main() {
+    std::string file_path = __FILE__;
+    std::string dir_path = file_path.substr(0, file_path.rfind("\\"));
+    std::ifstream input_file{ dir_path + "\\input.txt" };
+    std::string myline;
+
+    if (!input_file.is_open()) {
+        spdlog::error(fmt::format("failed to open input"));
+        return -1;
+    }
+
+    unsigned sum = 0;
+    while (std::getline(input_file, myline)) {
+        // TODO: check performance with deferent solution
+        // size_t last_index = str.find_last_not_of("0123456789");
+
+        auto is_digit = [](char c) -> bool { return std::isdigit(c); };
+        auto first = std::find_if(myline.cbegin(), myline.cend(), is_digit);
+        auto last = std::find_if(myline.rbegin(), myline.rend(), is_digit);
+        sum += std::stoi(fmt::format("{}{}\n", *first, *last));
+    }
+    fmt::print("sum: {}\n", sum);
+    return 0;
+}
